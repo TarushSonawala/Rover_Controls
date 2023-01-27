a9d5610d534fd2009c9de1c03cce2fb831cd829f
Tarush Sonawala <96786117+TarushSonawala@users.noreply.github.com>
2023-01-27 05:51:55 +0530

    Update SM_ESP32.cpp

--- ESP32_Rover_Final_Codes/Science Module/SM_ESP32.cpp
+++ ESP32_Rover_Final_Codes/Science Module/SM_ESP32.cpp
@@ -2,15 +2,19 @@
 #include <iostream>
 #include <string>
 
-HardwareSerial Receiver(0);
+HardwareSerial Receiver(1);
 
-std::string buffer = "";
+std::string buffer = "";//This creates a new variable called "buffer" of type std::string and assigns an empty string to it.
 
 const int freq = 5000;
 const int channel1 = 0;
 const int channel2 = 1;
 const int channel3 = 2;
 const int resolution = 8;
+unsigned long previous_millis = 0;
+const int debounce_time = 1000;  // debounce time in milliseconds
+bool current_direction = LOW;
+int stepsPerRevolution = 200;
 
 #define relay1 21
 #define relay2 19
@@ -18,24 +22,35 @@ const int resolution = 8;
 #define relay4 5
 #define water_heater 17
 #define stepper_step 23
-#define stepper_dir 22 ///
+#define stepper_dir 22
 #define auger_pwm 14
 #define auger_dir 27
 #define auger_rot_pwm 13
 #define auger_rot_dir 15
 #define senser_suit_pwm 33
 #define senser_suit_dir 32
+#define limit_switch_top 34
 
 char sm = '0';
+void IRAM_ATTR function_ISR() {
+  ets_printf("Limit Switch Triggered");
+  for (int i = 0; i < 500; i++)
+  {
+    digitalWrite(auger_dir, HIGH);
+    digitalWrite(auger_rot_dir, HIGH);
+    ledcWrite(channel1, 255);
+    ledcWrite(channel2, 255);
+  }
 
+}
 void setup()
 {
 
   Serial.begin(115200);
-  Receiver.begin(115200);
+  Receiver.begin(115200, 25, 26); //(baud rate,protocol,Tx,Rx)
 
   delay(1000);
-
+  //  attachInterrupt(limit_switch_top, function_ISR, FALLING);
   pinMode(relay1, OUTPUT);          // relay 1
   pinMode(relay2, OUTPUT);          // relay 2
   pinMode(relay3, OUTPUT);          // relay 3
@@ -46,6 +61,7 @@ void setup()
   pinMode(auger_dir, OUTPUT);       // auger actuation direction
   pinMode(auger_rot_dir, OUTPUT);   // auger rotation direction
   pinMode(senser_suit_dir, OUTPUT); // sensor suite direction
+  pinMode(limit_switch_top, INPUT); //Limit Switch
 
   ledcSetup(channel1, freq, resolution);
   ledcSetup(channel2, freq, resolution);
@@ -88,6 +104,7 @@ void SMControl(char buffer)
   switch (buffer)
   {
     case '0': // safety
+    
       ledcWrite(channel1, 0);
       ledcWrite(channel2, 0);
       ledcWrite(channel3, 0);
@@ -97,7 +114,6 @@ void SMControl(char buffer)
       digitalWrite(relay4, LOW);
       digitalWrite(water_heater, LOW);
       digitalWrite(stepper_step, LOW);
-      digitalWrite(stepper_dir, LOW);
       digitalWrite(auger_dir, LOW);
       digitalWrite(auger_rot_dir, LOW);
       digitalWrite(senser_suit_dir, LOW);
@@ -116,22 +132,38 @@ void SMControl(char buffer)
       break;
 
     case '3': // servo rotate
-      Serial.println("Hey its 3");
-
-      digitalWrite(stepper_step, HIGH);
-      delayMicroseconds(5000);
-      digitalWrite(stepper_step, LOW);
-      delayMicroseconds(5000);
+      Serial.println(current_direction);
+      //      digitalWrite(stepper_dir, current_direction);
+      for (int x = 0; x < 3; x++)
+      {
+        digitalWrite(stepper_step, HIGH);
+        delayMicroseconds(500);
+        digitalWrite(stepper_step, LOW);
+        delayMicroseconds(500);
+      }
+      //      digitalWrite(stepper_step, HIGH);
+      //      delayMicroseconds(200);
+      //      digitalWrite(stepper_step, LOW);
+      //      delayMicroseconds(200);
       break;
 
     case '4': // servo direction toggle
-      digitalWrite(stepper_dir, !digitalRead(stepper_dir));
-      delay(100);
-      // may have to add delay
+      Serial.println(current_direction);
+      if (millis() - previous_millis >= debounce_time) {
+        previous_millis = millis();
+        if (current_direction == LOW) {
+          digitalWrite(stepper_dir, HIGH);
+          current_direction = HIGH;
+        }
+        else {
+          digitalWrite(stepper_dir, LOW);
+          current_direction = LOW;
+        }
+      }
       break;
 
     case '5': // auger down with rotation
-      Serial.println("Hey its 5");
+      Serial.println("Hey its 500 mo-fkers");
       for (int i = 0; i < 500; i++) {
 
 
@@ -145,11 +177,10 @@ void SMControl(char buffer)
     case '6': // auger up]
       for (int i = 0; i < 500; i++)
       {
-
         digitalWrite(auger_dir, LOW);
         digitalWrite(auger_rot_dir, LOW);
-        ledcWrite(channel1, 255);
-        ledcWrite(channel2, 0);
+        ledcWrite(channel1, 0);
+        ledcWrite(channel2, 255);
       }
       break;
 
@@ -159,8 +190,8 @@ void SMControl(char buffer)
 
         digitalWrite(auger_dir, LOW);
         digitalWrite(auger_rot_dir, LOW);
-        ledcWrite(channel1, 0);
-        ledcWrite(channel2, 255);
+        ledcWrite(channel1, 255);
+        ledcWrite(channel2, 0);
       }
       break;
 
